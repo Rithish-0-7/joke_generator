bde3714b92f0873dadca05fdffcb7629210336e1
Rithish <rithishk108@gmail.com>
2025-03-23 11:29:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const button = document.getElementById("button");
+const jokecontent = document.getElementById("jokecontent");
+const joke = document.getElementById("joke");
+
+
+button.onclick = () => {
+    axios.get('https://official-joke-api.appspot.com/random_joke')
+    .then(function(response){
+        button.textContent = "Next";
+        joke.textContent ="";
+        jokecontent.textContent = response.data.setup;
+        setTimeout(function(){
+            joke.textContent = response.data.punchline;
+            // var img = document.getElementById("image")
+            // img.src= 'not_funny.jpg';
+        },1500);
+        
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+};
